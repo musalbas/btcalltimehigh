c0d303b9975ae01f7a366fe40bda8bd4c35ae3a2
musalbas <musalbas@riseup.net>
2013-12-05 21:45:53 +0000

    More fixes

--- server.py
+++ server.py
@@ -32,7 +32,30 @@ class PricePoller:
         # Dictionary of Bitcoin all time high prices
         self.price_history = {}
 
-    def _poll(self):
+    def _update_high(self, price, symbol, unixtime):
+        """Update self.price_history with a new all time high price. This works
+        such that there is a maximum of one all time high price item per
+        day."""
+        # Delete the current all time high price in self.price_history if it
+        # was reached on the same day as the new all time high price
+        day_seconds = 60 * 60 * 24
+        current_all_time_high = self.get_all_time_high()
+        if (current_all_time_high != -1 and
+            self.price_history[current_all_time_high][1]
+            > math.floor(time.time() / day_seconds) * day_seconds):
+            del self.price_history[current_all_time_high]
+
+        # Add price item to self.price_history
+        self.price_history[price] = (symbol, unixtime)
+
+    def get_all_time_high(self):
+        """Return the current all time high price."""
+        try:
+            return max(self.price_history.keys(), key=int)
+        except ValueError:
+            return -1
+
+    def poll(self):
         """Poll the Bitcoin price and update self.price_history if a new all
         high has been reached."""
         # Get markets data from bitcoincharts.com
@@ -43,29 +66,19 @@ class PricePoller:
         # price has been reached, by looking at each market's highest trade
         # during the day
         for market in markets_data:
-            if markets_data['high'] > self.get_all_time_high():
+            if (market['high'] > self.get_all_time_high()
+                and "USD" in market['symbol']
+                and market['symbol'] != "localbtcUSD"):
             # New high, update self.price_history
-                self._update_high(markets_data['high'], markets_data['symbol'],
+                self._update_high(market['high'], market['symbol'],
                                   time.time())
 
-    def _update_high(self, price, symbol, time):
-        """Update self.price_history with a new all time high price. This works
-        such that there is a maximum of one all time high price item per
-        day."""
-        # Delete the current all time high price in self.price_history if it
-        # was reached on the same day as the new all time high price
-        day_seconds = 60 * 60 * 24
-        current_all_time_high = self.get_all_time_high()
-        if (self.price_history[current_all_time_high][1]
-            > math.floor(time.time() / day_seconds) * day_seconds):
-            del self.price_history[current_all_time_high]
-
-        # Add price item to self.price_history
-        self.price_history[price] = (symbol, time)
-
-    def get_all_time_high(self):
-        """Return the current all time high price."""
-        return max(self.price_history.keys(), key=int)
+    def run(self):
+        """A loop that calls self.poll() every self.interval seconds."""
+        while True:
+            self.poll()
+            return self.price_history
+            time.sleep(self.interval)
 
 
 class homepage:
