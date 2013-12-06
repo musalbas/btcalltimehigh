b3578fb5c74cb705e4430280fe055f632b09a08e
musalbas <musalbas@riseup.net>
2013-12-06 01:16:27 +0000

    Finish up PricePoller

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ except ImportError:
     import json
 
 import math
+import os
 import urllib
 import web
 
@@ -26,18 +27,34 @@ class PricePoller:
     """
 
     def __init__(self, data_file, interval=900):
-        self.data_file = data_file
-        self.interval = interval
+        self._data_file = data_file
+        self._interval = interval
 
-        # Dictionary of Bitcoin all time high prices
-        self.price_history = {}
+        # Load the dictionary of Bitcoin all time high prices from the data
+        # file
+        try:
+            file_handle = open(data_file, 'r')
+            self.price_history = json.load(file_handle)
+            file_handle.close()
+        except IOError, e:
+            if e.errno != 2:
+                raise
+            self.price_history = {}
+
+    def _save(self):
+        """Save the price history dictionary to the data file in JSON
+        format."""
+        file_handle = open(self._data_file, 'w')
+        json.dump(self.price_history, file_handle)
+        file_handle.close()
 
     def _update_high(self, price, symbol, unixtime):
-        """Update self.price_history with a new all time high price. This works
-        such that there is a maximum of one all time high price item per
-        day."""
-        # Delete the current all time high price in self.price_history if it
-        # was reached on the same day as the new all time high price
+        """Update the price history dictionary with a new all time high price.
+        This works such that there is a maximum of one all time high price item
+        per day."""
+        # Delete the current all time high price in the price history
+        # dictionary if it was reached on the same day as the new all time high
+        # price
         day_seconds = 60 * 60 * 24
         current_all_time_high = self.get_all_time_high()
         if (current_all_time_high != -1 and
@@ -45,13 +62,16 @@ class PricePoller:
             > math.floor(time.time() / day_seconds) * day_seconds):
             del self.price_history[current_all_time_high]
 
-        # Add price item to self.price_history
+        # Add the price item to price history dictionary
         self.price_history[price] = (symbol, unixtime)
+
+        # Save the updated price history dictionary to the data file
+        self._save()
 
     def get_all_time_high(self):
         """Return the current all time high price."""
         try:
-            return max(self.price_history.keys(), key=int)
+            return max(self.price_history.keys(), key=float)
         except ValueError:
             return -1
 
@@ -69,16 +89,15 @@ class PricePoller:
             if (market['high'] > self.get_all_time_high()
                 and "USD" in market['symbol']
                 and market['symbol'] != "localbtcUSD"):
-            # New high, update self.price_history
+                # New high, update price history dictionary
                 self._update_high(market['high'], market['symbol'],
                                   time.time())
 
     def run(self):
-        """A loop that calls self.poll() every self.interval seconds."""
+        """A loop that calls self.poll before every interval."""
         while True:
             self.poll()
-            return self.price_history
-            time.sleep(self.interval)
+            time.sleep(self._interval)
 
 
 class homepage:
@@ -86,6 +105,7 @@ class homepage:
 
     def GET(self):
         """Handle GET requests."""
+        return pricepoller.get_all_time_high()
         return "sup?"
 
 # Setup web.py URL mapping
@@ -93,7 +113,14 @@ urls = (
     '/', 'homepage',
 )
 
-# Start web.py application
-app = web.application(urls, globals())
+# Start the application
 if __name__ == "__main__":
+    # Start the price poller in a separate thread
+    module_dirname = os.path.dirname(os.path.abspath(__file__))
+    pricepoller_data_file = os.path.join(module_dirname, "price_history.json")
+    pricepoller = PricePoller(pricepoller_data_file)
+    thread.start_new_thread(pricepoller.run, ())
+
+    # Start the web.py application
+    app = web.application(urls, globals())
     app.run()
