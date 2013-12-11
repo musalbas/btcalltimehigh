e338bc44ec1a6feb74c61966a08c1d8c8c8815fd
Mustafa Al-Bassam <musalbas@riseup.net>
2013-12-11 15:36:50 +0000

    fix download pattern

--- price_history_import_tools/getcsvs.sh
+++ price_history_import_tools/getcsvs.sh
@@ -1,3 +1,3 @@
 # Download the USD trade data CSV files from bitcoincharts.com using wget.
-wget http://api.bitcoincharts.com/v1/csv/ -m -r -np -A *USD.csv -R localbtcUSD.csv,*.html -c -nd -P csvs
+wget http://api.bitcoincharts.com/v1/csv/ -m -r -np -A *USD.csv -R localbtcUSD.csv,*.html,*LRUSD.csv -c -nd -P csvs
 rm -fv `find csvs -type f -size 0`
