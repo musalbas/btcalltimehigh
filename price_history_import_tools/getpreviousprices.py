cb786d9dfa569bc231ba3e7b3b09a45e95036fe4
Fares Alaboud <fares.alaboud@gmail.com>
2013-12-13 12:45:39 +0000

    fix syntax line 16, missing brackets

--- price_history_import_tools/getpreviousprices.py
+++ price_history_import_tools/getpreviousprices.py
@@ -10,16 +10,16 @@ with con:
                 #print "Loop: " + str(loop_number)
                 #loop_number = loop_number + 1
                 print row
-                # If the price found on the frow is bigger than the currently r$
+                # If the price found on the frow is bigger than the currently recorded highest price
                 if row[2] > current_highest_price:
-                        # If this is not the first row AND the day of the price$
-                        if (current_highest_price != -1 and daily_highest_price$
+                        # If this is not the first row AND the day of the price in the row is the next day of the price
+                        if (current_highest_price != -1 and daily_highest_prices[current_highest_price][1] > math.floor((row[1] /day_seconds) * day_seconds)):
                                 # Delete the recorded highest price of that day
                                 del daily_highest_prices[current_highest_price]
                         # Overwrite the price of the row
                         current_highest_price = row[2]
                         # Add the highest price into the dictionary
-                        daily_highest_prices[current_highest_price] = (row[1], $
+                        daily_highest_prices[current_highest_price] = (row[1], row[4])
                         # Print out the highest price
                         print daily_highest_prices
                 # Get the next row
