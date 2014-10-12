d63cca9d0ea6d20cf420726162782c8ca6994917
Mustafa <mus@musalbas.com>
2014-10-12 12:04:44 +0100

    enable production mode by default

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """Configuration constants."""
 
 # Configure CherryPy web server here
-CHERRYPY_CONFIG = {'server.socket_host': "127.0.0.1",
-                   'server.socket_port': 9081}
+CHERRYPY_CONFIG = {'server.socket_host': '127.0.0.1',
+                   'server.socket_port': 9081,
+                   'environment': 'production'}
